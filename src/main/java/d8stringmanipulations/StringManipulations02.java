4bbc775be77827d76f0e3611d8378bfbacc11415
Emre Sani <emresani1998@gmail.com>
2024-01-22 09:34:38 +0100

    Version 1.1

---
+++ src/main/java/d8stringmanipulations/StringManipulations02.java
@@ -0,0 +1,17 @@
+package d8stringmanipulations;
+
+public class StringManipulations02 {
+    public static void main(String[] args) {
+        //Ornek 1: “s” String’indeki “money” kelimesini “dollar” kelimesine ceviriniz.
+
+        String s = "learn Java earn money";
+
+        String s1 = s.replace("money","dollar");
+
+        System.out.println(s1);
+
+
+
+
+    }
+}

d4b556788644be6a2e08fb4e4e6ffbe9f65fbeea
Emre Sani <emresani1998@gmail.com>
2024-02-26 16:06:42 +0300

    q14 q15 solved

--- src/main/java/javaquestions/string_manipulations/Q14.java
+++ src/main/java/javaquestions/string_manipulations/Q14.java
@@ -2,4 +2,19 @@ package javaquestions.string_manipulations;
 
 public class Q14 {
     //Type code to check if a String has a specific single character or not in three different ways
+    public static void main(String[] args) {
+        String str = "Emre Sani";
+
+        // Method 1: Using contains()
+        boolean result1 = str.contains("E");
+        System.out.println("Method 1: " + result1);
+
+        // Method 2: Using regex and length
+        boolean result2 = str.replaceAll("[^E]", "").length() > 0;
+        System.out.println("Method 2: " + result2);
+
+        // Method 3: Using indexOf()
+        boolean result3 = str.indexOf('E') != -1;
+        System.out.println("Method 3: " + result3);
+    }
 }
--- src/main/java/javaquestions/string_manipulations/Q15.java
+++ src/main/java/javaquestions/string_manipulations/Q15.java
@@ -4,4 +4,19 @@ public class Q15 {
     //) String shirtPrice = ‘$12.99’;
     //String bookPrice = ‘$35.99’;
     //Type code to find the sum of the shirt and book prices
+    public static void main(String[] args) {
+        String shirtPrice = "$12.99";
+        String bookPrice = "$35.99";
+        String s = shirtPrice.replaceAll("[$]", "");
+        String b = bookPrice.replaceAll("[$]", "");
+        double x = Double.valueOf(s);
+        System.out.println(x);
+        double y = Double.valueOf(b);
+        System.out.println(y);
+        double sum = x + y;
+
+        // Format the output to display only two decimal places
+        System.out.printf("Total price is $%.2f\n", sum);
+
+    }
 }
